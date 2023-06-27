453ee34627c1cec41632cfa6b2492d80e065f939
paperized <ivanlogreco99@outlook.it>
2023-06-27 15:11:02 +0200

    Unused import

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,4 +1,4 @@
-import {component$, h} from "@builder.io/qwik";
+import {component$} from "@builder.io/qwik";
 import type {Project} from "~/model/project";
 import {getTagColor} from "~/data/projects-data";
 
@@ -8,20 +8,21 @@ export default component$((props: { projects: Project[] }) => {
       {props.projects.map((project) => {
         const ProjectTag = (project.link ? 'a' : 'div');
         return (
-          <ProjectTag key={project.id} href={project.link ? project.link : undefined} target={project.link ? '_blank' : undefined}
-               class={["flex rounded-2xl bg-[#6b5f8d52] border-[1px] border-[#0079ff66] box-border p-5 overflow-hidden",
-                 project.link ? 'hover:translate-y-1' : '']}
-               style="box-shadow: 2px 1px 13px 2px #0079ff75;">
+          <ProjectTag key={project.id} href={project.link ? project.link : undefined}
+                      target={project.link ? '_blank' : undefined}
+                      class={["flex rounded-2xl bg-[#6b5f8d52] border-[1px] border-[#0079ff66] box-border p-5 overflow-hidden",
+                        project.link ? 'hover:translate-y-1' : '']}
+                      style="box-shadow: 2px 1px 13px 2px #0079ff75;">
             {project.preview_image && (
               <img class="object-contain shrink-0 h-40" src={project.preview_image} alt="Project Image"/>
             )}
             <div class={[project.preview_image ? 'md:pl-6' : '']}>
               <div class="flex flex-wrap gap-x-8">
                 <span
-                   class={[
-                     "font-bold text-xl mb-2",
-                     project.link ? "bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-transparent" : ""
-                   ]}>{project.name}</span>
+                  class={[
+                    "font-bold text-xl mb-2",
+                    project.link ? "bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-transparent" : ""
+                  ]}>{project.name}</span>
                 <span
                   class="bg-gray-200 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{project.project_owner}</span>
 
