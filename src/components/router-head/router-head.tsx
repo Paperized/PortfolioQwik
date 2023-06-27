e778fbbea3e3f530b9f14a972db409eef016096a
paperized <ivanlogreco99@outlook.it>
2023-06-27 18:34:11 +0200

    Fixed path favicon android

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -15,12 +15,12 @@ export const RouterHead = component$(() => {
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <link rel="apple-touch-icon" sizes="180x180" href="https://paperized-dev.com/favicon/apple-touch-icon.png"/>
-      <link rel="apple-touch-icon-precomposed" sizes="180x180" href="https://paperized-dev.com/favicon/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="https://paperized-dev.com/favicon/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="https://paperized-dev.com/favicon/favicon-16x16.png"/>
-      <link rel="manifest" href="https://paperized-dev.com/favicon/site.webmanifest"/>
-      <link rel="mask-icon" href="https://paperized-dev.com/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+      <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png"/>
+      <link rel="apple-touch-icon-precomposed" href="/favicon/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+      <link rel="manifest" href="/favicon/site.webmanifest"/>
+      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
       <meta name="msapplication-TileColor" content="#da532c"/>
       <meta name="theme-color" content="#000000"/>
 
