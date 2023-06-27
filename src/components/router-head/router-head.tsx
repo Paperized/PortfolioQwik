6d3020edeba3b05d07a6da16064f5589325e167f
paperized <ivanlogreco99@outlook.it>
2023-06-27 18:13:47 +0200

    Absolute urls in some favicons

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -15,8 +15,8 @@ export const RouterHead = component$(() => {
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
-      <link rel="apple-touch-icon-precomposed" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="https://paperized-dev.com/favicon/apple-touch-icon.png"/>
+      <link rel="apple-touch-icon-precomposed" sizes="180x180" href="https://paperized-dev.com/favicon/apple-touch-icon.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
       <link rel="manifest" href="/favicon/site.webmanifest"/>
