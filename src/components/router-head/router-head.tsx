dcc6fee0f2a5cad3313f6fc14f6bcaf2432965e8
paperized <ivanlogreco99@outlook.it>
2023-06-27 18:36:50 +0200

    Last try and I'm giving up on android home favicons

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -16,7 +16,6 @@ export const RouterHead = component$(() => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
       <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png"/>
-      <link rel="apple-touch-icon-precomposed" href="/favicon/apple-touch-icon.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
       <link rel="manifest" href="/favicon/site.webmanifest"/>
